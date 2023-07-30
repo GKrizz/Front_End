4eaba26fb6104392bde2af9f1c1dcbdaf489336a
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2023-07-30 10:31:05 +0530

    commited

---
+++ JS/Form-validation/js/script.js
@@ -0,0 +1,40 @@
+// window.alert()
+// window.open("https://cs50.harvard.edu/python/2022/weeks/5/")
+// Window.close();
+
+// switch case------------
+
+// var week=3
+// switch(week){
+//     case 1:
+//         document.write("monday");
+//         break;
+//     case 2:
+//         document.write("tues");
+//         break;
+//     case 3:
+//         document.write("wednws");
+//         break;
+//     case 4:
+//         document.write("thurs");
+//         break;
+
+
+// for (i=5;i>=1;i--){
+//     document.write(i)
+//     document.write("<br>")
+// }
+// i=1
+// while(i<=5)
+// {
+//     document.write(i);
+//     document.write("<br>");
+//     i++;
+// }
+// i=1
+// do{
+//     document.write(i);
+//     document.write("<br>");
+//     i++;
+// }while(i<=8)
+
