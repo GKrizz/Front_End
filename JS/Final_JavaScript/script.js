845548427d4d6dd1e4ae1ddc5ef8fd8db358b0b7
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2024-07-08 18:21:14 +0530

    DOM EventListener

--- JS/Final_JavaScript/script.js
+++ JS/Final_JavaScript/script.js
@@ -61,3 +61,20 @@ console.log(result)
 // Select paragraph tag by using ID
 var para=document.getElementById("one")
 console.log(para.textContent)
+
+// eventhandler
+var change=document.getElementById("change");
+change.addEventListener("click",function(){
+    console.log("clicked");
+    para.textContent="Bye World";
+});
+
+//Add two numbers
+var button = document.querySelector(".result_class");
+var output = document.getElementById("result");
+button.addEventListener("click", function() {
+    var number_1 =parseFloat(document.getElementById("num1").value);
+    var number_2 =parseFloat(document.getElementById("num2").value);
+    var add = number_1 + number_2;
+    output.textContent = "Result : " + add;
+});
