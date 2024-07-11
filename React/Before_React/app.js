0b2145e45392a51341487b1cc8376370f88756ae
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2024-07-11 12:17:16 +0530

    Objects

--- React/Before_React/app.js
+++ React/Before_React/app.js
@@ -16,11 +16,28 @@ DoSomething()*/
 // Ternary Operator is used to write a short if else statement.
 // Syntax: condition ? expression1:expression2
 
+/*
 let age=12;
 let message=age>=18?"Eligible":"Not Eligible";
 console.log(message);
 
 const component=()=>{
     return age>=18 ? <div>Eligible</div>:<div>Not Eligible</div>
-}
+}*/
 
+// -----------------------------|   Objects   |-----------------------------
+
+// Objects are used to store data in key value pairs.
+// Syntax: {key:value,key:value,key:value}
+// Objects are mutable.
+// Objects are reference type.
+
+const person={
+    name:"John",
+    age:12,
+    isMarried:false,
+};
+console.log(person);
+
+const person2={...person,name:"gobal"}
+console.log(person2);
