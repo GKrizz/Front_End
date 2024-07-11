1ecc0e48616917348ea33655c6ad310a695862bf
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2024-07-11 12:49:36 +0530

    MAP , REDUCE AND Filter

--- React/Before_React/app.js
+++ React/Before_React/app.js
@@ -32,6 +32,8 @@ const component=()=>{
 // Objects are mutable.
 // Objects are reference type.
 
+/*
+
 const person={
     name:"John",
     age:12,
@@ -41,3 +43,29 @@ console.log(person);
 
 const person2={...person,name:"gobal"}
 console.log(person2);
+
+*/
+
+// -----------------------------|   Map , Filter ,Reduce  |-----------------------------
+const marks = [35,55,89,25,45]
+console.log(marks);
+
+// Map is used to iterate over an array and return a new array.
+// Syntax: array.map((item,index)=>{return item;});
+
+const grade= marks.map((e)=> e>=45? "pass":"fail") // fail , pass
+console.log("grade",grade)
+
+// Filter is used to iterate over an array and return a new array.
+// Syntax: array.filter((item,index)=>{return item;});
+
+const pass = marks.filter((e)=> e>=45)
+console.log("pass",pass)
+
+// Reduce is used to iterate over each element of an array and return a single value.
+// Syntax: array.reduce((acc,item)=>{return acc+item;},0);
+
+const sum = marks.reduce((acc , e ) => {
+    return acc + e ;
+},0 );
+console.log("sum",sum)
