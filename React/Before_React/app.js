1a2e059dc82a57a3f729952ac42e26c22db7556c
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2024-07-11 12:05:36 +0530

    Ternary Operator

--- React/Before_React/app.js
+++ React/Before_React/app.js
@@ -1,3 +1,6 @@
+// -----------------------------|   Arrow Function   |-----------------------------
+
+/*
 export default function DoSomething(){// In normal function we use |default|.
     console.log("Hello Function");
 }
@@ -6,6 +9,18 @@ DoSomething()
 export const DoSomething=()=>{//In arrow function we use |export| alone. 
     console.log("Hello Arrow Function");
 }
-DoSomething()
+DoSomething()*/
 
+// -----------------------------|   Ternary Operator   |-----------------------------
+
+// Ternary Operator is used to write a short if else statement.
+// Syntax: condition ? expression1:expression2
+
+let age=12;
+let message=age>=18?"Eligible":"Not Eligible";
+console.log(message);
+
+const component=()=>{
+    return age>=18 ? <div>Eligible</div>:<div>Not Eligible</div>
+}
 
